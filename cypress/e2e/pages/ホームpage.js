c1897d520764a46d7d16da1dc4981ef85f687233
Akinobu Okazaki <a-okazaki@sios.com>
2023-07-24 15:18:26 +0900

    pageの要素を追加

--- cypress/e2e/pages/ホームpage.js
+++ cypress/e2e/pages/ホームpage.js
@@ -1,10 +1,16 @@
 class ホームPage {
     elements = {
-        宿泊予約Tab: () => cy.contains("宿泊予約")
+        宿泊予約Tab: () => cy.contains("宿泊予約"),
+        会員登録Tab: () => cy.contains("会員登録"),
+        ログインBtn: () => cy.contains("ログイン")
     }
 
     宿泊予約Tabを選択する() {
         this.elements.宿泊予約Tab().click();
+    }
+
+    会員登録Tabを選択する() {
+        this.elements.会員登録Tab().click();
     }
 }
 
